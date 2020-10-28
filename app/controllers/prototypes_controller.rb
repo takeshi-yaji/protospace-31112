a98c423ee297aee996e418a2174462134730fb18
takeshi-yaji <72482665+takeshi-yaji@users.noreply.github.com>
2020-10-28 14:46:41 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!,  only: [:new, :create, :edit, :update,:destroy]
-  before_action :move_to_index, only: [:edit]
+  before_action :move_to_index, except: [:index, :show]
   def index
     @prototypes = Prototype.all
   end
@@ -44,6 +44,7 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end 
+
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
